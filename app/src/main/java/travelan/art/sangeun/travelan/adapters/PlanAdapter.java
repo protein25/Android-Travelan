994d5d2f3d6acd8d7b42d39f78c58dadd0282687
choikyubum <bbulbum@gmail.com>
2018-06-13 14:36:02 +0900

    plan

--- app/src/main/java/travelan/art/sangeun/travelan/adapters/PlanAdapter.java
+++ app/src/main/java/travelan/art/sangeun/travelan/adapters/PlanAdapter.java
@@ -1,6 +1,7 @@
 package travelan.art.sangeun.travelan.adapters;
 
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,21 @@ import android.widget.TextView;
 import java.util.List;
 
 import travelan.art.sangeun.travelan.R;
+import travelan.art.sangeun.travelan.dialog.MapLocation;
+import travelan.art.sangeun.travelan.dialog.MapLocationDialog;
+import travelan.art.sangeun.travelan.dialog.OnMapSelectListener;
 import travelan.art.sangeun.travelan.models.Plan;
 
 public class PlanAdapter extends RecyclerView.Adapter {
-    private static final int ATTRIBUTE_COMMON = 0;
-    private static final int ATTRIBUTE_TRANSPORT = 1;
+    private static final int ATTRIBUTE_ADD = 0;
+    private static final int ATTRIBUTE_COMMON = 1;
+    private static final int ATTRIBUTE_TRANSPORT = 2;
 
     public List<Plan> items;
+    private FragmentManager fragmentManager;
 
-    public PlanAdapter(List<Plan> items) {
+    public PlanAdapter(FragmentManager fragmentManager, List<Plan> items) {
+        this.fragmentManager = fragmentManager;
         this.items = items;
     }
 
@@ -28,6 +35,9 @@ public class PlanAdapter extends RecyclerView.Adapter {
         View view;
 
         switch (viewType) {
+            case ATTRIBUTE_ADD:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_plan_add, parent, false);
+                return new ViewHolderAdd(view);
             case ATTRIBUTE_TRANSPORT:
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_plan_transportation, parent, false);
                 return new ViewHolderTransport(view);
@@ -40,25 +50,46 @@ public class PlanAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Plan item = items.get(position);
+        int viewType = getItemViewType(position);
 
-        if (getItemViewType(position) == ATTRIBUTE_COMMON) {
-            ViewHolderCommon viewHolder = (ViewHolderCommon) holder;
-            viewHolder.title.setText(item.title);
-            viewHolder.address.setText(item.address);
-            viewHolder.tel.setText(item.tel);
-        } else {
+        if (viewType == ATTRIBUTE_ADD) {
+            ViewHolderAdd viewHolder = (ViewHolderAdd) holder;
+            viewHolder.add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MapLocationDialog mapLocationDialog = new MapLocationDialog();
+                    mapLocationDialog.setOnMapSelectedListener(new OnMapSelectListener() {
+                        @Override
+                        public void onSelect(MapLocation mapLocation) {
+
+                        }
+                    });
+                    mapLocationDialog.show(fragmentManager, "mapLocationDialog");
+                }
+            });
+        } else if (viewType == ATTRIBUTE_TRANSPORT) {
             ViewHolderTransport viewHolder = (ViewHolderTransport) holder;
             viewHolder.time.setText(item.time);
             viewHolder.way.setText(item.way);
             viewHolder.origin.setText(item.origin);
             viewHolder.route.setText(item.route);
             viewHolder.destination.setText(item.destination);
+        } else {
+            ViewHolderCommon viewHolder = (ViewHolderCommon) holder;
+            viewHolder.title.setText(item.title);
+            viewHolder.address.setText(item.address);
+            viewHolder.tel.setText(item.tel);
         }
     }
 
     @Override
     public int getItemViewType(int position) {
         Plan item = items.get(position);
+
+        if (item.id < 0) {
+            return ATTRIBUTE_ADD;
+        }
+
         if (item.attributeType.equals("transportation")) {
             return ATTRIBUTE_TRANSPORT;
         }
@@ -69,6 +100,15 @@ public class PlanAdapter extends RecyclerView.Adapter {
     @Override
     public int getItemCount() {
         return items.size();
+    }
+
+    class ViewHolderAdd extends RecyclerView.ViewHolder {
+        TextView add;
+
+        public ViewHolderAdd(View itemView) {
+            super(itemView);
+            add = itemView.findViewById(R.id.add);
+        }
     }
 
     class ViewHolderCommon extends RecyclerView.ViewHolder {
---
+++ app/src/main/java/travelan/art/sangeun/travelan/dialog/MapLocation.java
@@ -0,0 +1,10 @@
+package travelan.art.sangeun.travelan.dialog;
+
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapLocation {
+    public LatLng latlng;
+    public String address;
+    public String poi;
+}
